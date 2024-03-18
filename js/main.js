6910d526453672b699912c649ec7b62ac7382a13
val <walkbefaith@gmail.com>
2024-03-18 15:59:23 -0500

    resultado

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //Convertidor de peso Colombiano a euro, dolar, peso mexicanos, 
 //peso argentino, peso chileno, soles.
 
-let pesoColombiano = parseFloat(prompt("Ingrese el valor que desea convertir"))
+
 const TASA_CAMBIO_DOLAR = 0.00026
 const TASA_CAMBIO_EURO = 0.00024
 const TASA_CAMBIO_MEXICANO = 0.0043
@@ -9,7 +9,6 @@ const TASA_CAMBIO_ARGENTINO = 0.22
 const TASA_CAMBIO_CHILENO = 0.24
 const TASA_CAMBIO_PERUANO = 0.00095
 
-let monedaObjetivo = prompt("Selecciona la moneda a la que deseas hacer el cambio:\nUSD - Dolar americado\nEUR - Euro\nMXN - Peso Mexicano\nARS - Peso Argentino\nCLP - Peso chileno\nSOL - Sol peruano")
 
 function convertir(valor, moneda){
 
@@ -40,6 +39,12 @@ function convertir(valor, moneda){
 
 }
 
+let otrosCambios = true;
+while (otrosCambios){
+let pesoColombiano = parseFloat(prompt("Ingrese el valor que desea convertir"))
+let monedaObjetivo = prompt("Selecciona la moneda a la que deseas hacer el cambio:\nUSD - Dolar americado\nEUR - Euro\nMXN - Peso Mexicano\nARS - Peso Argentino\nCLP - Peso chileno\nSOL - Sol peruano")
+
+
 let resultado = convertir(pesoColombiano, monedaObjetivo)
 
 if (resultado === null ) {
@@ -49,4 +54,5 @@ if (resultado === null ) {
     alert (pesoColombiano + " equivalen a " + resultado + " " + monedaObjetivo)
 }
 
-
+otrosCambios = confirm("¿Desea hacer otra conversión?");
+}
