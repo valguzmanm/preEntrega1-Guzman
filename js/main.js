26006006433e4ac0f6d8c30d6bc65d13ea868ee3
val <walkbefaith@gmail.com>
2024-03-17 22:50:15 -0500

    cambios

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@
 let pesoColombiano = parseFloat(prompt("Ingrese el valor que desea convertir"))
 const TASA_CAMBIO_DOLAR = 0.00026
 const TASA_CAMBIO_EURO = 0.00024
+const TASA_CAMBIO_MEXICANO = 0.0043
+const TASA_CAMBIO_ARGENTINO = 0.22
+const TASA_CAMBIO_CHILENO = 0.24
+const TASA_CAMBIO_PERUANO = 0.00095
 
 let monedaObjetivo = prompt("Selecciona la moneda a la que deseas hacer el cambio:\nUSD - Dolar americado\nEUR - Euro\nMXN - Peso Mexicano\nARS - Peso Argentino\nCLP - Peso chileno\nSOL - Sol peruano")
 
@@ -13,11 +17,23 @@ function convertir(valor, moneda){
 
     switch (moneda) {
         case "USD":
-            return valor * TASA_CAMBIO_DOLAR
+            return valor * TASA_CAMBIO_DOLAR;
         
+        case "EUR":
+             return valor * TASA_CAMBIO_EURO;
 
-            
-    
+        case "MXN":
+             return valor * TASA_CAMBIO_MEXICANO;
+
+        case "ARS":
+             return valor * TASA_CAMBIO_ARGENTINO;
+
+        case "CLP":
+             return valor * TASA_CAMBIO_CHILENO;
+
+        case "SOL":
+             return valor * TASA_CAMBIO_PERUANO;
+
         default: 
            return null
     }
