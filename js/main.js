8af47a5621828d1d97c67f0ca882dc2eef5176e7
val <walkbefaith@gmail.com>
2024-03-18 20:09:31 -0500

    ultimo cambio

--- js/main.js
+++ js/main.js
@@ -40,25 +40,38 @@ function convertir(valor, moneda){
 
 }
 
+
 let otrosCambios = true;
-while (otrosCambios){
-let pesoColombiano = parseFloat(prompt("Ingrese el valor que desea convertir"))
-let monedaObjetivo = prompt("Selecciona la moneda a la que deseas hacer el cambio:\nUSD - Dolar americado\nEUR - Euro\nMXN - Peso Mexicano\nARS - Peso Argentino\nCLP - Peso chileno\nSOL - Sol peruano")
+
+while (otrosCambios) {
+
+    let pesoColombiano;
+
+    do {
+        pesoColombiano = parseFloat(prompt("Ingrese el valor que desea convertir"))
+        if (isNaN(pesoColombiano)) {
+            alert("Ese valor no es válido, intentelo de nuevo.")
+        }
+    } while (isNaN(pesoColombiano));
 
 
-let resultado = convertir(pesoColombiano, monedaObjetivo)
 
-if (resultado === null ) {
-    alert("Esta conversión no se puede realizar")
-} else {
-    resultado = resultado.toFixed(2)
-    alert (pesoColombiano + " equivalen a " + resultado + " " + monedaObjetivo)
-}
+    let monedaObjetivo = prompt("Selecciona la moneda a la que deseas hacer el cambio:\nUSD - Dolar americado\nEUR - Euro\nMXN - Peso Mexicano\nARS - Peso Argentino\nCLP - Peso chileno\nSOL - Sol peruano")
 
-otrosCambios = confirm("¿Desea hacer otra conversión?");
 
-if(otrosCambios === false){
-    alert("¡Gracias por usar nuestros servicios, nos vemos pronto!")
-}
+    let resultado = convertir(pesoColombiano, monedaObjetivo)
+
+    if (resultado === null ) {
+        alert("Esta conversión no se puede realizar")
+    } else {
+        resultado = resultado.toFixed(2)
+        alert (pesoColombiano + " equivalen a " + resultado + " " + monedaObjetivo)
+    }
+
+    otrosCambios = confirm("¿Desea hacer otra conversión?");
+
+    if(otrosCambios === false){
+        alert("¡Gracias por usar nuestros servicios, nos vemos pronto!")
+    }
 }
 
